0d30fdc044f3277d36d1728719cdd4035171eb24
Mradul Shrivastava <mradsulshrivastavams@gmail.com>
2024-08-21 11:14:20 -0700

    changes done in frontend

---
+++ Frontend/src/assets/Data.js
@@ -0,0 +1,29 @@
+export const Data = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "London", "Berlin", "Madrid"],
+      correctAnswer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Earth", "Mars", "Jupiter", "Saturn"],
+      correctAnswer: "Mars",
+    },
+    {
+      question: "What is the largest ocean on Earth?",
+      options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+      correctAnswer: "Pacific Ocean",
+    },
+    {
+      question: "Who wrote 'To Kill a Mockingbird'?",
+      options: ["Harper Lee", "Mark Twain", "F. Scott Fitzgerald", "Ernest Hemingway"],
+      correctAnswer: "Harper Lee",
+    },
+    {
+      question: "What is the boiling point of water?",
+      options: ["90°C", "100°C", "120°C", "150°C"],
+      correctAnswer: "100°C",
+    }
+  ];
+  
+ 
